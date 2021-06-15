c9eceea893f459b400ec76da7d196f2d5aefd4a0
Jon <jon.bosque.hernando@gmail.com>
2021-06-15 18:02:07 +0200

    fix damage_smite add attacks per round

--- hunters_mark.py
+++ hunters_mark.py
@@ -6,6 +6,7 @@ target_ac = 15
 # Character
 strenght = +3
 proficiency = +3
+attacked_round = 3
 # State
 advantage = False
 
@@ -30,18 +31,21 @@ damage_attack = (prob_hit * (damage_dice + damage_extra)
 
 # two attacks
 damage_hunter = 2*damage_attack  # 2 attacks
-print(
-    f'P(Enemy Hit): {phit_enemy} - P(Keep Con): {psave} - P(Enemy Hit AND Keep Con): {pnotbreak}')
+print(f'P(Enemy Hit): {phit_enemy} - P(Keep Con): {psave}', end='')
+print(' - P(Enemy Hit AND Keep Con): {pnotbreak}')
 print(f'Round: {0} - Damage: {damage_hunter:.2f}')
 for i in range(1, rounds):
-    damage_hunter += pnotbreak**i * 2 * damage_attack
-    print(
-        f'Round: {i} - Damage: {pnotbreak**i * 2*damage_attack:.2f}|{damage_hunter:.2f}')
+    damage_hunter += pnotbreak**(i*attacked_round) * 2 * damage_attack
+    print(f'Round: {i} - Damage: {pnotbreak**i * 2*damage_attack:.2f}', end='')
+    print(f'|{damage_hunter:.2f}')
 
 print(f'Hunter Total Damage: {damage_hunter:.2f}')
 
 # 1st Level Divine Smite
 prob_hit, prob_crit = p_hit(target_ac, hit_dc, advantage)
 damage_dice = damage_roll(divine_smite)
-damage_smite = damage_dice + prob_crit/prob_hit * 2*damage_dice
+damage_smite = damage_dice + prob_crit * 2*damage_dice
 print(f'Divine Smite Damage: {damage_smite:.2f}')
+
+# Here we compare the damage output per spellslot.
+# Spent in hunter's mark or divine smite
